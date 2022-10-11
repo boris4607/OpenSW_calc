56e89024d633d4ad9f53ea1ef6b57085b8b3be17
DESKTOP-5FJ2C27\JIHO <kjc8985@gmail.com>
2022-10-12 00:46:23 +0900

    프로젝트 생성
    프로젝트 생성 및 깃 이그노어 작성

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 간단한계산기_20162881
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
